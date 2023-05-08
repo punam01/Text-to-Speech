256afbe89757b1acf637f50680ca92a5d54f4ba3
Punam Kumavat <punamkumavat01@gmail.com>
2023-05-08 13:47:13 +0530

    refactor: beautified

--- main.js
+++ main.js
@@ -11,90 +11,85 @@ const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
 const body = document.querySelector('body');
 
-//Browser identifier
-// Firefox 1.0+
-
-
 // Init voices array
 let voices = [];
 
 const getVoices = () => {
-  voices = synth.getVoices();
+    voices = synth.getVoices();
 
-  // Loop through voices and create an option for each one
-  voices.forEach(voice => {
-    // Create option element
-    const option = document.createElement('option');
-    // Fill option with voice and language
-    option.textContent = voice.name + '(' + voice.lang + ')';
+    // Loop through voices and create an option for each one
+    voices.forEach(voice => {
+        // Create option element
+        const option = document.createElement('option');
+        // Fill option with voice and language
+        option.textContent = voice.name + '(' + voice.lang + ')';
 
-    // Set needed option attributes
-    option.setAttribute('data-lang', voice.lang);
-    option.setAttribute('data-name', voice.name);
-    voiceSelect.appendChild(option);
-  });
+        // Set needed option attributes
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+    });
 };
 
 
-//Fix for duplication, run code depending on the browser
- if (synth.onvoiceschanged !== undefined) {
-        synth.onvoiceschanged = getVoices;
-    };
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+};
 
 // Speak
 const speak = () => {
-  // Check if speaking
-  if (synth.speaking) {
-    console.error('Already speaking...');
-    return;
-  }
-  if (textInput.value !== '') {
-    // Add background animation
-    body.style.background = '#141414 url(img/wave.gif)';
-    body.style.backgroundRepeat = 'repeat-x';
-    body.style.backgroundSize = '100% 100%';
+    // Check if speaking
+    if (synth.speaking) {
+        console.error('Already speaking...');
+        return;
+    }
+    if (textInput.value !== '') {
+        // Add background animation
+        body.style.background = '#141414 url(img/wave.gif)';
+        body.style.backgroundRepeat = 'repeat-x';
+        body.style.backgroundSize = '100% 100%';
 
-    // Get speak text
-    const speakText = new SpeechSynthesisUtterance(textInput.value);
+        // Get speak text
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
 
-    // Speak end
-    speakText.onend = e => {
-      console.log('Done speaking...');
-      body.style.background = '#141414';
-    };
+        // Speak end
+        speakText.onend = e => {
+            console.log('Done speaking...');
+            body.style.background = '#141414';
+        };
 
-    // Speak error
-    speakText.onerror = e => {
-      console.error('Something went wrong');
-    };
+        // Speak error
+        speakText.onerror = e => {
+            console.error('Something went wrong');
+        };
 
-    // Selected voice
-    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
-      'data-name'
-    );
+        // Selected voice
+        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+            'data-name'
+        );
 
-    // Loop through voices
-    voices.forEach(voice => {
-      if (voice.name === selectedVoice) {
-        speakText.voice = voice;
-      }
-    });
+        // Loop through voices
+        voices.forEach(voice => {
+            if (voice.name === selectedVoice) {
+                speakText.voice = voice;
+            }
+        });
 
-    // Set pitch and rate
-    speakText.rate = rate.value;
-    speakText.pitch = pitch.value;
-    // Speak
-    synth.speak(speakText);
-  }
+        // Set pitch and rate
+        speakText.rate = rate.value;
+        speakText.pitch = pitch.value;
+        // Speak
+        synth.speak(speakText);
+    }
 };
 
 // EVENT LISTENERS
 
 // Text form submit
 textForm.addEventListener('submit', e => {
-  e.preventDefault();
-  speak();
-  textInput.blur();
+    e.preventDefault();
+    speak();
+    textInput.blur();
 });
 
 // Rate value change
